c45fce30137bf1c647cdfefc9a5d07374e15911b
Huehuetips <eadony35@gmail.com>
2024-11-19 15:57:32 -0600

    Vista casi completa y funcional

--- modules/tracking_guides/models/models.py
+++ modules/tracking_guides/models/models.py
@@ -11,16 +11,17 @@ class history_guide(models.Model):
     phone_number = fields.Char(string="Número de teléfono")
     address = fields.Text(string="Dirección de entrega")
     description = fields.Text("Descripción de entrega")
-    shipping_details = fields.Text()
+    shipping_details_ids = fields.One2many('tracking_guides.shipping_details', 'history_guide_id', string="Detalles de Envío")
 
 
 class shipping_details(models.Model):
     _name = 'tracking_guides.shipping_details'
     _description = 'tracking_guides.shipping_details'
     
+    history_guide_id = fields.Many2one('tracking_guides.history_guide', string="Guía de Historia")
     selection_shipping_type = [
-        ('sobre', '1'),
-        ('caja', '2'),
+        ('1', 'Sobre'),
+        ('2', 'Caja'),
     ]
     pieces = fields.Integer(string="Cantidad de piezas")
     shipping_type = fields.Selection(selection_shipping_type, string="Tipo de envío")
