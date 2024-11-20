db40adb93c6a056f8cbd89ebd43ab7c5eeb69693
Huehuetips <eadony35@gmail.com>
2024-11-20 09:12:46 -0600

    <b>CAMBIOS NO ÚTILES, COMMIT PARA LUEGO HACER UN REVERT</b>

--- modules/tracking_guides/models/models.py
+++ modules/tracking_guides/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class history_guide(models.Model):
@@ -13,6 +13,32 @@ class history_guide(models.Model):
     description = fields.Text("Descripción de entrega")
     shipping_details_ids = fields.One2many('tracking_guides.shipping_details', 'history_guide_id', string="Detalles de Envío")
 
+    def call_api_and_save(self):
+        # Realiza la llamada a la API
+        api_response = self.call_api()  # Implementa tu lógica para la llamada a la API
+
+        if api_response.get('success'):  # Verifica si la llamada fue exitosa
+            self.create()  # Guarda el registro si la llamada fue exitosa
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'tracking_guides.history_guide',
+                'view_mode': 'form',
+                'res_id': self.id,
+                'target': 'current',
+            }
+        else:
+            # Manejo de errores, mostrar mensaje de error
+            return {
+                'warning': {
+                    'title': _("Error"),
+                    'message': _("La llamada a la API falló."),
+                }
+            }
+
+    def call_api(self):
+        # Implementa la lógica para llamar a la API
+        # Retorna un diccionario con el resultado
+        return {'success': True}  # Ejemplo de respuesta
 
 class shipping_details(models.Model):
     _name = 'tracking_guides.shipping_details'
@@ -24,5 +50,5 @@ class shipping_details(models.Model):
         ('2', 'Caja'),
     ]
     pieces = fields.Integer(string="Cantidad de piezas")
-    shipping_type = fields.Selection(selection_shipping_type, string="Tipo de envío")
-    weight = fields.Float(string="Peso")
+    shipping_type = fields.Selection(selection_shipping_type, string="Tipo de envío", default='2')
+    weight = fields.Float(string="Peso (Lb)")
